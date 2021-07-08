8ce568fe45a68895baa92b94acc3d1437624ab6b
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2021-07-08 16:31:27 +0900

    add camel/camel-jmx.java

---
+++ camel/camel-jmx.java
@@ -0,0 +1,33 @@
+///usr/bin/env jbang --javaagent=org.jolokia:jolokia-jvm:1.6.2:agent "$0" "$@" ; exit $?
+//DEPS org.apache.camel:camel-bom:3.11.0@pom
+//DEPS org.apache.camel:camel-core
+//DEPS org.apache.camel:camel-management
+//DEPS org.apache.camel:camel-main
+//DEPS org.apache.camel:camel-stream
+//DEPS org.slf4j:slf4j-nop:1.7.31
+
+// Camel imports
+import org.apache.camel.*;
+import org.apache.camel.builder.*;
+import org.apache.camel.main.*;
+import org.apache.camel.spi.*;
+import static org.apache.camel.builder.PredicateBuilder.*;
+
+import static java.lang.System.*;
+
+class camel_jmx {
+
+    public static void main(String... args) throws Exception {
+        out.println("Running Camel route...");
+
+        Main main = new Main();
+        main.configure().addRoutesBuilder(new RouteBuilder() {
+            public void configure() throws Exception {
+                from("timer:hello?period=3000")
+                    .setBody().constant("Hello Camel!")
+                    .to("stream:out");
+            }
+        });
+        main.run();
+    }
+}
